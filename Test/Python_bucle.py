14da01665adffb8f3ae451b43df7e25c8c9a19af
Benjamin <benjamincanas1@gmail.com>
2017-09-25 18:52:23 -0300

    Mi app Django Girls, primer commit

---
+++ Test/Python_bucle.py
@@ -0,0 +1,9 @@
+def comprar(cosas_para_comprar):
+    print("Comprar " + cosas_para_comprar)
+
+print("Lista de compras:\n")
+lista_de_compras = ["Papas", "Leche", "Avena", "Cereal", "agua"]
+for cosas_para_comprar in lista_de_compras:
+    comprar(cosas_para_comprar)
+    print("Y tambien\n")
+añadir_item = 
---
+++ Test/Python_intro.py
@@ -0,0 +1,7 @@
+def hola(name):
+    if name == "Benja":
+        name1 = "Benjamin"
+        print(name1 + ", buenas tardes, que tenga un buen dia")
+    else:
+        print("disculpe, ¿como se llama?")
+hola("Benja")
